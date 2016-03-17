f2acf6d78c57572a225fc382ebd9e82006485c21
akselCakmak <akselcakmak@gmail.com>
2016-03-17 02:12:45 +0000

    Create editDevice.php

---
+++ Combine/editDevice.php
@@ -0,0 +1,20 @@
+$servername="localhost";
+    $username="root";
+    $password="";
+    $database="MOBILEDEVICELENDINGPROJECT";
+    $conn=New mysqli($servername,$username,$password,$database);
+<!DOCTYPE HTML>
+<html> 
+
+<?php>
+  //Connection to the database
+  $servername="localhost";
+  $username="root";
+  $password="";
+  $database="MOBILEDEVICELENDINGPROJECT";
+  $conn=New mysqli($servername,$username,$password,$database);
+
+<php>
+
+
+</html>
