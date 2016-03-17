145de4984f0bdb652fb5d65046f5b260b3387ea7
akselCakmak <akselcakmak@gmail.com>
2016-03-17 01:35:47 +0000

    again just pseudocode for this page
    this has pseudocode
    will be completed with smthing relevant no worries

--- Combine/removeDevice.php
+++ Combine/removeDevice.php
@@ -1,1 +1,10 @@
+<!--Just some quick pseudocode bcs i forgot the syntax lol 
+I ll complete this bit by bit when i have the time don t worry :p
+1st
+Get the id of the object we want to edit
+2nd
+Make an SQL connection to delete the object that has that id, in the database
+3nd
+Go back to the displayDevice page, which is now updated
 
+-->
