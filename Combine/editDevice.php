ca017aeac8248ac39df484a447706db7aa287f92
akselCakmak <akselcakmak@gmail.com>
2016-03-17 01:33:35 +0000

    some pre-arrangements
    just wrote the basic layout of this page;
    (that is, some basic pseudocode)
    will complete it

--- Combine/editDevice.php
+++ Combine/editDevice.php
@@ -1,1 +1,16 @@
+<!--Just some quick pseudocode bcs i forgot the syntax lol 
+I ll complete this bit by bit when i have the time don t worry :p
+
+1st
+Get the id of the object we want to edit
+
+2nd
+Make a form of what you want to write for that object
+
+3nd
+Make an SQL connection to modify the database with the submitted form
+
+4th
+Go back to the displayDevice page, which is now updated
+-->
 
