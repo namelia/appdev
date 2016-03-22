6bb1280a4f65dd73df210a9019e6475030e32db2
Nancy Amelia <nancyamelia97@Hotmail.com>
2016-03-22 18:06:21 +0000

    new files such as search table new table display with filter automatic updates

---
+++ Combine/config.php
@@ -0,0 +1,7 @@
+<?php
+$servername='localhost';
+$username='root';
+$password='';
+$database='mobiledevicelendingproject';
+$conn=New mysqli($servername,$username,$password,$database)
+?>
---
+++ Combine/tabledisplay2.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 3/12/2016
+ * Time: 5:06 PM
+ */
+include 'tabledisplay.php';
