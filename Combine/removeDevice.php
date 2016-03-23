360838094fc131ffed1f7b2d8298e85f55cb6efe
akselCakmak <akselcakmak@gmail.com>
2016-03-23 11:47:30 +0000

    Update removeDevice.php

--- Combine/removeDevice.php
+++ Combine/removeDevice.php
@@ -16,24 +16,32 @@ Go back to the displayDevice page, which is now updated
 <html> 
 <body>
 
-<?php> 
+<?php 
 //Get the object's ID with GET which holds the value of the parameter sent earlier (with the line editDevicehp.php?deviceID=$id in the file displaytable.php)
 $deviceID = $_GET['deviceID'] ; //is this how you do it though? :/
 
-//Connection to the database
-$servername="localhost";
-$username="root";
-$password="";
-$database="MOBILEDEVICELENDINGPROJECT";
-$conn=New mysqli($servername,$username,$password,$database);
+$bdd = new PDO('mysql:host=localhost;dbname=MOBILEDEVICELENDINGPROJECT;charset=utf8', 'root', 'root');
+  
+  //Retrieve the value from the parameter passed: (again, lol, this is the second time that the ID gets retrieved by a page with this parameter passing method)
+  $deviceID = $_GET['deviceID'] ;
+  
+  //Now that everything is set up (we have the connection + the variable values), let s put all of this in the database
+  //idk what to write before the sql query, but I know what to write inside:
+  //UPDATE deviceTable SET name=$newName , color=newColor,  details=newDetails, OS = newOS WHERE id=$deviceID
+  $req = $bdd->prepare('DELETE FROM objects  WHERE id = :idDevice');
+  $req->execute(array(
+	  'idDevice' => $deviceID
+	));
 
 //Now that we're connected, and that we know the ID of the device, let's delete that object:
 //Idk how to do the exact query, bcs i learned the old syntax, but i know exactly what the actual sql code is:
 //DELETE FROM deviceTable WHERE id=$deviceID
 
 //Last thing to do:
-//I don t remember how, but here, just add an automatic redirection to the displayTable page
+?>
 
-
+<?php
+  
+?> 
 </body>
 </html>
