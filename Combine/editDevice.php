a8bb47eb18cd78a60baeb679590f8318eb0a4781
akselCakmak <akselcakmak@gmail.com>
2016-03-23 11:35:08 +0000

    Update editDevice.php

--- Combine/editDevice.php
+++ Combine/editDevice.php
@@ -4,11 +4,8 @@
 
 <?php>
   //Connection to the database
-  $servername="localhost";
-  $username="root";
-  $password="";
-  $database="MOBILEDEVICELENDINGPROJECT";
-  $conn=New mysqli($servername,$username,$password,$database);
+  
+  $bdd = new PDO('mysql:host=localhost;dbname=MOBILEDEVICELENDINGPROJECT;charset=utf8', 'root', 'root');
   
   //Retrieve the value from the parameter passed: (again, lol, this is the second time that the ID gets retrieved by a page with this parameter passing method)
   $deviceID = $_GET['deviceID'] ;
@@ -21,7 +18,14 @@
   //Now that everything is set up (we have the connection + the variable values), let s put all of this in the database
   //idk what to write before the sql query, but I know what to write inside:
   //UPDATE deviceTable SET name=$newName , color=newColor,  details=newDetails, OS = newOS WHERE id=$deviceID
-  
+  $req = $bdd->prepare('UPDATE objects SET name = :new_name, OS = :new_OS, color = new_color, details = new_details WHERE id = :idDevice');
+  $req->execute(array(
+	  'new_name' => $newName,
+	  'new_OS' => $newOS,
+	  'new_color' => $newColor,
+	  'new_details' => $newDetails,
+	  'idDevice' => $deviceID
+	));
   
   //Last thing:
   //I don t remember how, but here, just add an automatic redirection to the displayTable page
