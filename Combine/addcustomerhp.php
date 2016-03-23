ff75f6f80014692b20f9ea9bfcf4aca6f61d5929
Nancy Amelia <nancyamelia97@Hotmail.com>
2016-03-23 02:30:46 +0000

    put the details in one page also edited the config to remote redunduncy

--- Combine/addcustomerhp.php
+++ Combine/addcustomerhp.php
@@ -14,7 +14,7 @@
 <body>
 <?php include ("index.html") ?>
 <div class="container">
-    <form action="addcustomer.php" method="post">
+    <form action="addcustomerhp.php" method="post">
         Customers Name: <input  type="text" name="custname" required><br><br>
         Email address: <input  type="text" name="custemail" class="form-control required" type="email" required><br><br>
         Category:
@@ -29,10 +29,37 @@
         <br>
         <textarea name="details" rows="10" cols="20"></textarea>
         <br><br>
-        <input type= "submit" value="Submit" name="submit">
+        <input type= "submit" value="Submit" name="submitt">
     </form>
 
 </div>
 </body>
 
+<div id =responsecust>
+<?php
+include("config.php");
+if(isset($_POST['submitt']))
+{
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $cust_name = $_POST['custname'];
+    $email_ = $_POST['custemail'];
+    $category_ = $_POST['category'];
+    $details_ = $_POST['details'];
+    $sql = "INSERT INTO addcustomers (name,email,category,details) VALUES ('$cust_name','$email_','$category_','$details_')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo 'Details have been added to the database!';
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
+
+
+</div>
 </html>
--- Combine/additemhp.php
+++ Combine/additemhp.php
@@ -12,7 +12,7 @@
 
 
 <div class="container">
-<form action="additem.php" method="post">
+<form action="additemhp.php" method="post">
     Device Name: <input  type="text" name="devicename" required><br><br>
     ID: <input  type="text" name="ID" required><br><br>
     Color:
@@ -47,10 +47,35 @@
     <br>
     <textarea name="message"rows="10"cols="20"></textarea>
     <br><br>
-    <input type= "submit" value="Submit" name="submit">
+    <input type= "submit" value="Submit" name="submitt">
 </form>
 
 </div>
 </body>
+<div id ="responseitem">
+<?php
+include("config.php");
+if(isset($_POST['submitt']))
+{
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
+    $device_name = $_POST['devicename'];
+    $id_ = ($_POST['ID']);
+    $color_ = $_POST['color'];
+    $os_ = $_POST['OS'];
+    $details_ = $_POST['message'];
+    $sql = "INSERT INTO additem (name,id,color,OS,details) VALUES ('$device_name','$id_','$color_','$os_','$details_')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo 'Details have been added to the database!';
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
+</div>
 </html>
