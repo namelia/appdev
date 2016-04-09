4c9cdbe4d247d04989194640c405d3c0290f1ec8
Nancy Amelia <nancyamelia97@Hotmail.com>
2016-04-09 22:13:06 +0100

    removed color and added the filter by category

--- Combine/tableItem.php
+++ Combine/tableItem.php
@@ -78,19 +78,19 @@ BODY, TD:not(id=sidebar) {
 				<label>Items:</label>
 				<input type="text" name="string" id="string" value="<?php echo stripcslashes($_REQUEST["string"]); ?>" />
 			</div>
-			<div  class="col-xs-2">
-				<label>Color</label>
-				<select name="color">
+			<div  class="col-xs-3">
+				<label>category</label>
+				<select name="category" id="cat">
 				<option value="">--</option>
 				<?php
-					$query= $conn->query("SELECT * FROM $table GROUP BY color ORDER BY color")or die ('request "Could not execute SQL query" '.$sql);
+					$query= $conn->query("SELECT * FROM $table GROUP BY category ORDER BY category")or die ('request "Could not execute SQL query" '.$sql);
 					while ($row = $query->fetch_assoc()) {
-						echo "<option value='".$row["color"]."'".($row["color"]==$_REQUEST["color"] ? " selected" : "").">".$row["color"]."</option>";
+						echo "<option value='".$row["category"]."'".($row["category"]==$_REQUEST["category"] ? " selected" : "").">".$row["category"]."</option>";
 					}
 				?>
 				</select>
 			</div>
-			<div  class="col-xs-3">
+			<div  class="col-xs-2">
 				<input type="submit" name="button" id="button" value="Filter" />
 				</label>
 				<a href="tableItem.php"> reset</a>
@@ -103,34 +103,35 @@ BODY, TD:not(id=sidebar) {
 <br /><br />
 <table width="600" border="1" cellspacing="0" cellpadding="4" class="table table-striped table-hover table-responsive">
   <tr>
-    <td width="90" bgcolor="#CCCCCC"><strong>From date</strong></td>
-    <td width="95" bgcolor="#CCCCCC"><strong>To date</strong></td>
-    <td width="159" bgcolor="#CCCCCC"><strong>Name</strong></td>
-    <td width="191" bgcolor="#CCCCCC"><strong>Description</strong></td>
-    <td width="113" bgcolor="#CCCCCC"><strong>Color</strong></td>
-	  <td colspan="2" width="70" bgcolor="#CCCCCC"><strong>Manage</strong></td>
-	  <td width="50" bgcolor="#CCCCCC"><strong>Availability</strong></td>
+	  <td width="159" bgcolor="#CCCCCC"><strong>Category</strong></td>
+      <td width="90" bgcolor="#CCCCCC"><strong>From date</strong></td>
+      <td width="95" bgcolor="#CCCCCC"><strong>To date</strong/td>
+      <td width="159" bgcolor="#CCCCCC"><strong>Name</strong></td>
+      <td width="191" bgcolor="#CCCCCC"><strong>Description</strong></td>
+
+	  <td colspan="2" width="20" bgcolor="#CCCCCC"><strong>Manage</strong></td>
+	  <td width="30" bgcolor="#CCCCCC"><strong>Availability</strong></td>
   </tr>
 <?php
 
 if ($_REQUEST["string"]<>'') {
 	$search_string = " AND (name LIKE '%".mysqli_real_escape_string($conn,$_REQUEST["string"])."%' OR details LIKE '%".mysqli_real_escape_string($conn,$_REQUEST["string"])."%')";
 }
-if ($_REQUEST["color"]<>'') {
-	$search_color = " AND color='".mysqli_real_escape_string($conn,$_REQUEST["color"])."'";
+if ($_REQUEST["category"]<>'') {
+	$search_category = " AND category='".mysqli_real_escape_string($conn,$_REQUEST["category"])."'";
 }
 
 if ($_REQUEST["from"]<>'' and $_REQUEST["to"]<>'')
 {
-	$sql = "SELECT * FROM $table WHERE from_date >= '".mysqli_real_escape_string($conn,$_REQUEST["from"])."' AND to_date <= '".mysqli_real_escape_string($conn,$_REQUEST["to"])."'".$search_string.$search_color;
+	$sql = "SELECT * FROM $table WHERE from_date >= '".mysqli_real_escape_string($conn,$_REQUEST["from"])."' AND to_date <= '".mysqli_real_escape_string($conn,$_REQUEST["to"])."'".$search_string.$search_category;
 } else if ($_REQUEST["from"]<>'')
 {
-	$sql = "SELECT * FROM $table WHERE from_date >= '".mysqli_real_escape_string($conn,$_REQUEST["from"])."'".$search_string.$search_color;
+	$sql = "SELECT * FROM $table WHERE from_date >= '".mysqli_real_escape_string($conn,$_REQUEST["from"])."'".$search_string.$search_category;
 } else if ($_REQUEST["to"]<>'')
 {
-	$sql = "SELECT * FROM $table WHERE to_date <= '".mysqli_real_escape_string($conn,$_REQUEST["to"])."'".$search_string.$search_color;
+	$sql = "SELECT * FROM $table WHERE to_date <= '".mysqli_real_escape_string($conn,$_REQUEST["to"])."'".$search_string.$search_category;
 } else {
-	$sql = "SELECT * FROM $table WHERE id>0".$search_string.$search_color;
+	$sql = "SELECT * FROM $table WHERE id>0".$search_string.$search_category;
 }
 
 $sql_result =$conn->query($sql) or die ('request "Could not execute SQL query" '.$sql);
@@ -138,12 +139,13 @@ if (mysqli_num_rows($sql_result)>0) {
 	while ($row=$sql_result->fetch_assoc()) {
 ?>
   <tr>
-    <td><?php echo $row["from_date"]; ?></td>
-    <td><?php echo $row["to_date"]; ?></td>
-    <td><?php echo $row["name"]; ?></td>
-    <td><?php echo $row["details"]; ?></td>
-    <td><?php echo $row["color"]; ?></td>
-	  <td> <a href="editItem.php?id=<?php echo $row["id"]?>&name=<?php echo $row["name"]?>&details=<?php echo $row["details"]?>&color=<?php echo $row["color"]?>&OS=<?php echo $row["OS"]?>" class="group1"  >Edit</a> </td>
+	  <td><?php echo $row["category"]; ?></td>
+      <td><?php echo $row["from_date"]; ?></td>
+      <td><?php echo $row["to_date"]; ?></td>
+      <td><?php echo $row["name"]; ?></td>
+      <td><?php echo $row["details"]; ?></td>
+
+	  <td> <a href="editItem.php?id=<?php echo $row["id"]?>&name=<?php echo $row["name"]?>&details=<?php echo $row["details"]?>&category=<?php echo $row["category"]?>&OS=<?php echo $row["OS"]?>" class="group1"  >Edit</a> </td>
 	  <td> <a href="removeItem.php?id=<?php echo $row["id"] ?>">Remove</a> </td>
 	  <td> <form $row  id ='checkbox $i' action="" method="post">
 			  <input type="checkbox" name=<?php echo "box "+$row["id"]?> value="Available" class="group1" >
