5b1b0908b9817d5c8783c9732beda5771959f6b9
Pierce <zcabpgr@ucl.ac.uk>
2016-04-20 02:04:09 +0100

    2x LIE COMBO. Here's a revamp of the edit administrator details page.

--- Combine/profile.php
+++ Combine/profile.php
@@ -8,18 +8,18 @@
 <div id =sidebar class="visible">
     <?php include("sidebar.php"); ?>
 </div>
+
 <body>
-
-
 <div class="container">
     <form action="profile.php" method="post">
-        Name: <input   class ="tb5" type="text" name="name" required value="<?php echo $_SESSION['user_info']['name'] ?>" ><br><br>
-        Email address: <input  class ="form-control required tb5" type="email" name="email" required  value="<?php echo $_SESSION['user_info']['email'] ?>"><br><br>
-        Password: <input   class ="tb5" type="text" name="password"  value=""> <br><br>
+        Current password: <input class = "form-control required" type="password" name="oldpass"  value=""> <br><br>
+        Name: <input type="text" name="name" required value="<?php echo $_SESSION['user_info']['name'] ?>" ><br><br>
+        Email address: <input class ="form-control required" type="email" name="email" required  value="<?php echo $_SESSION['user_info']['email'] ?>"><br><br>
+        New password (leave blank to keep the same): <input type = "password" name="password"  value=""> <br><br>
+        Confirm new password: <input type = "password" name="password2"  value=""> <br><br>
         <br><br>
         <input type= "submit" value="Update" name="Update">
     </form>
-
 </div>
 </body>
 
@@ -30,26 +30,58 @@ if(isset($_POST['Update']))
         die("Connection failed: " . $conn->connect_error);
     }
     //$someID = $_GET['id'];
-    $email_ = $_POST['email'];
-    $name_ = $_POST['name'];
-    $passhash_ = md5($_POST['password']);
-    $id_ = $_SESSION['user_info']['id'];
+    $oldpass_ = md5($_POST['oldpass']);
+    if ($oldpass_ == $_SESSION['user_info']['passhash']) {
+        $email_ = $_POST['email'];
+        $name_ = $_POST['name'];
+        $passhash_ = md5($_POST['password']);
+        $passhash2_ = md5($_POST['password2']);
+        if ($passhash_ == md5('')) {
+            $passhash_ = $oldpass_;
+        }
+        if ($passhash2_ == md5('')) {
+            $passhash2_ = $oldpass_;
+        }
+        if ($passhash2_ == $passhash_) {
+            $id_ = $_SESSION['user_info']['id'];
 
-    $sql = "UPDATE login SET name = '$name_', email ='$email_' ,passhash='$passhash_'WHERE id = $id_";
-    if ($conn->query($sql) === TRUE) {
+            $sql = "UPDATE login SET name = '$name_', email ='$email_' ,passhash='$passhash_'WHERE id = $id_";
+            if ($conn->query($sql) === TRUE) {
+                $query2= $conn->query("SELECT * FROM login WHERE `id` = '$id_'") or die ('request "Could not execute SQL query" '.$sql);
+                $user= $query2->fetch_assoc();
+                if(!empty($user)) {
+                    $_SESSION['user_info'] = $user;
+                }
+                echo '<div id="boxes">
+                  <div id="dialog" class="window">
+    
+                  <h1>Details have been modified!</h1>
+                  </div>
+                  <div id="mask"></div>
+                  </div>';
 
-        echo '<div id="boxes">
-              <div id="dialog" class="window">
-
-              <h1>Details have been modified!</h1>
-              </div>
-              <div id="mask"></div>
-              </div>';
-
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+            $conn->close();
+        }
+        else {
+            echo '<div id="boxes">
+                <div id="dialog" class="window">
+                <h1>New passwords do not match...</h1>
+                </div>
+                <div id="mask"></div>
+                </div>';
+        }
     }
-    $conn->close();
+    else {
+        echo '<div id="boxes">
+                <div id="dialog" class="window">
+                <h1>Old password does not match records...</h1>
+                </div>
+                <div id="mask"></div>
+                </div>';
+    }
 }
 ?>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
